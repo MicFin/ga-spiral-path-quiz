be95068a7fd7a3fa04d7fc9dd26cc510ddc567b9
Michael Finneran <mrfinneran@gmail.com>
2013-10-01 09:35:09 -0400

    completed it for the example array, i commented it out as a attempted to make a formula that could accept other sized arrays

--- quiz.rb
+++ quiz.rb
@@ -1,1 +1,31 @@
 # Your code goes here
+array = [ [1,2,3], [8,9,4], [7,6,5] ]
+
+
+new_array = []
+count = array.count
+
+new_array << array[count-count] << array[count-]
+
+
+
+# one way
+# new_array = []
+# new_array << array.shift
+# new_array = new_array.flatten
+# new_array << array[0].pop
+# new_array = new_array.flatten
+# new_array << array[1].pop
+# new_array = new_array.flatten
+# new_array << array[1].pop
+# new_array = new_array.flatten
+# new_array << array[1]
+# new_array = new_array.flatten
+# new_array << array[0].shift
+# new_array = new_array.flatten
+# new_array << array[0]
+# new_array = new_array.flatten
+
+p new_array
+p array 
+
